54f20345a4fcdc93b207c5579ff1c0de353225c8
mix23456 <mixsax2@gmail.com>
2024-01-26 10:45:54 +0700

    new: assessment delete

--- src/services/assessment.service.tsx
+++ src/services/assessment.service.tsx
@@ -2,6 +2,7 @@ import { AssessmentResult } from "@app/interfaces/assessment.interface";
 import {
   assessmentfindAll,
   assessmentupdate,
+  assessmentdelete,
 } from "@app/serverinfo/serverinfo";
 import axios from "axios";
 
@@ -33,8 +34,30 @@ export default class AssessmentServices {
       return res.data["result"];
     } catch (e) {
       console.log(e);
-      // localStorage.removeItem("jwt");
-      // window.location.reload();
+      localStorage.removeItem("jwt");
+      window.location.reload();
+      return e;
+    }
+  }
+
+  static async delete(id: string) {
+    const headers = {
+      Authorization: "Bearer " + localStorage.getItem("jwt"),
+      "Content-Type": "application/json",
+    };
+    try {
+      const res = await axios.put(
+        `${assessmentdelete}/${id}`,
+        {},
+        {
+          headers,
+        }
+      );
+      return res.data["result"];
+    } catch (e) {
+      console.log(e);
+      localStorage.removeItem("jwt");
+      window.location.reload();
       return e;
     }
   }
